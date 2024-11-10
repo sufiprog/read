264b275d38c21cd43d818ac3f682c04c2d7ecdc1
Muhammad Sufyan <122455815+sufiprog@users.noreply.github.com>
2024-11-10 16:59:10 +0000

    4th commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import localFont from "next/font/local";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
